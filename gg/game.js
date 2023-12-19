893edc9efce762496eb9887ec1cc3c13bc4eea38
willow <willowcarter42@gmail.com>
2023-12-18 19:51:23 -0700

    Add files via upload

---
+++ gg/game.js
@@ -0,0 +1,11 @@
+function drawloop(){
+    ctx.beginPath();
+    ctx.strokeStyle = "red";
+    var dotx = ((jlx + 1)/2) * screenwidth;
+    var doty = ((jly+1)/2 * screenheight)
+    ctx.moveTo(dotx,doty);
+    ctx.lineTo(dotx + (jrx*50), doty + (jry*50));
+    ctx.stroke();
+    ctx.closePath();
+     requestAnimationFrame(drawloop);
+}
