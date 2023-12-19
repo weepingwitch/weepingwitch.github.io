90093119b3bf4cfb4f19133314bbcdf2a5fa6621
willow <willowcarter42@gmail.com>
2023-12-18 20:01:08 -0700

    Update game.js

--- gg/game.js
+++ gg/game.js
@@ -2,9 +2,9 @@ function drawloop(){
     ctx.beginPath();
     ctx.strokeStyle = "red";
     var dotx = ((jlx + 1)/2) * screenwidth;
-    var doty = (((-jly)+1)/2 * screenheight)
+    var doty = ((jly + 1)/2) * screenheight;
     ctx.moveTo(dotx,doty);
-    ctx.lineTo(dotx + (jrx*50), doty + (-jry*50));
+    ctx.lineTo(dotx + (jrx*50), doty - (jry*50));
     ctx.stroke();
     ctx.closePath();
      requestAnimationFrame(drawloop);
