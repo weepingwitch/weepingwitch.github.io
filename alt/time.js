6403161a707e87720067d12dfc372d4089ee84d5
willow <willowcarter42@gmail.com>
2024-01-01 15:30:18 -0700

    Add files via upload

---
+++ alt/time.js
@@ -0,0 +1,32 @@
+var tdiv = document.getElementById("timedata");
+function getTime(){
+    var curAccess = new Date();
+
+    tdiv.innerHTML += "currently: " + curAccess.toUTCString();
+
+
+    tdiv.innerHTML += "<BR>";
+
+    var curh = curAccess.getUTCHours();
+    tdiv.innerHTML += "hour " + curh + "<BR>";
+
+
+    if (localStorage.hasOwnProperty("lastViewed")){
+        //get duration time
+        var olddatestr = localStorage.getItem("lastViewed");
+        var olddate = new Date(olddatestr);
+        var elapsed = curAccess - olddate;
+        tdiv.innerHTML += "elapsed: " + elapsed;
+
+      
+    }
+    else{
+        //first time !!!
+        tdiv.innerHTML += "first time viewing!";
+
+    }
+
+
+
+    localStorage.setItem("lastViewed",curAccess);
+}
