67eb9fecab203b785b58cce0a4b1434f339a9412
willow <willowcarter42@gmail.com>
2025-12-23 21:12:01 -0700

    Add files via upload

---
+++ wice/engine.js
@@ -0,0 +1,92 @@
+
+            // Allocate some memory for the moodule. Initial size is specified in pages of 16 kiB.
+const memory = new WebAssembly.Memory( { initial: 16 } );
+
+// Everything we want to pass to our module during initialisation. The names of the keys
+// are not important, so long as our WASM module knows to look for them.
+const imports = {
+    js: {
+        mem: memory,
+        log: (arg) => {console.log(arg);return arg}
+    }
+};
+
+const byteArray = new Uint8ClampedArray( memory.buffer, 0, 512 * 512 * 4 );
+var frloop;
+
+var encoded = "... contents of encoded from above ...";
+
+
+
+function decode(encoded) {
+    var binaryString =  atob(encoded);
+    var bytes = new Uint8Array(binaryString.length);
+    for (var i = 0; i < binaryString.length; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
+    }
+    return bytes.buffer;
+}
+
+async function engine(){
+    fetch('./main.wasm')
+    .then( response => response.arrayBuffer() )
+    .then( bytes => WebAssembly.compile( bytes ) )
+    .then( module => new WebAssembly.Instance( module, imports ) )
+    .then( instance => {
+
+       
+         // Set the pixel data in the module's memory
+         frloop = instance.exports;
+        const res = frloop.blank();
+        frloop.init();
+        
+
+       
+
+    });
+}
+
+
+    var img;
+    var canvas;
+    var debugdiv;
+    var ctx;
+    var frame=0;
+
+    initcanvas();
+
+        // Put the mod  ule's memory into an array suitable for use in ImageData.
+        // Allocate width x height x number of colour planes (RGBA = 4)
+        
+
+    renderframe();
+     engine();
+
+    function initcanvas(){
+        // Get the canvas element from the DOM
+         canvas = document.getElementById('c');
+         debugdiv = document.getElementById('d');
+
+        // Get a 2D graphics context for the canvas
+         ctx = canvas.getContext('2d');
+    }
+
+function renderframe(){
+// Create an ImageData instance from the array
+
+         img = new ImageData( byteArray, 512, 512 );
+
+        // Put the image data into the canvas
+        ctx.putImageData( img, 0, 0 );
+        debugdiv.innerText = "image rendered, frame " + frame;
+        if (frloop != null){
+   // frloop.run();
+   frloop.setp((frame/512)+1,(frame%512));
+        }       
+        
+        frame += 1;
+        
+        //console.log(byteArray);
+        requestAnimationFrame(renderframe);
+        
+}
